13b3ecce2558efd9fce1a9577aebd5428404c3b3
David Schlenker <ironinstrumentals@gmail.com>
2019-05-13 10:18:53 -0600

    Updates

--- precache-manifest.94d6165e8eeb162bfd34ce184023e451.js
+++ precache-manifest.94d6165e8eeb162bfd34ce184023e451.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "f17fb3089131571e109d665f1b5d7eaa",
+    "revision": "7692359c365cb5542aa0d7446e09cfb3",
     "url": "/e-commerce/index.html"
   },
   {
-    "revision": "ec44f92e0e84a22fc007",
+    "revision": "4346ac198c20f143f789",
     "url": "/e-commerce/static/css/main.5f641995.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/e-commerce/static/js/2.a51b7850.chunk.js"
   },
   {
-    "revision": "ec44f92e0e84a22fc007",
-    "url": "/e-commerce/static/js/main.a951011a.chunk.js"
+    "revision": "4346ac198c20f143f789",
+    "url": "/e-commerce/static/js/main.a24ac11b.chunk.js"
   },
   {
     "revision": "290256ac1007569aecdb",
