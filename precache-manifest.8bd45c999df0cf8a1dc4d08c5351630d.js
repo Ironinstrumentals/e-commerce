19ae7722d18744edee526c1002eeec13f782ea26
David Schlenker <ironinstrumentals@gmail.com>
2019-05-15 09:52:10 -0600

    Updates

--- precache-manifest.8bd45c999df0cf8a1dc4d08c5351630d.js
+++ precache-manifest.8bd45c999df0cf8a1dc4d08c5351630d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "18e282e30e4fabf99a7efcb6f52e4ee5",
+    "revision": "221eaba376d1fb4f32fe3441112793b0",
     "url": "/e-commerce/index.html"
   },
   {
-    "revision": "0f862599781425220f1d",
-    "url": "/e-commerce/static/css/main.76987052.chunk.css"
+    "revision": "a48da1e4b37c29f3a834",
+    "url": "/e-commerce/static/css/main.d61e86b3.chunk.css"
   },
   {
     "revision": "01eb777069963381e227",
     "url": "/e-commerce/static/js/2.a51b7850.chunk.js"
   },
   {
-    "revision": "0f862599781425220f1d",
-    "url": "/e-commerce/static/js/main.b0580bd7.chunk.js"
+    "revision": "a48da1e4b37c29f3a834",
+    "url": "/e-commerce/static/js/main.142c4802.chunk.js"
   },
   {
     "revision": "290256ac1007569aecdb",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.0/workbox-sw.js");
 
 importScripts(
-  "/e-commerce/precache-manifest.fbe632eaa864dd9c88de2735a5701117.js"
+  "/e-commerce/precache-manifest.8bd45c999df0cf8a1dc4d08c5351630d.js"
 );
 
 self.addEventListener('message', (event) => {
