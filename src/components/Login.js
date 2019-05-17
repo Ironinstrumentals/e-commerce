d58f2d6134d3407cc849d8c801cb317e6547568a
David Schlenker <ironinstrumentals@gmail.com>
2019-05-17 08:03:42 -0600

    #0028 - Fixed Login

--- src/components/Login.js
+++ src/components/Login.js
@@ -29,7 +29,7 @@ export class Login extends Component {
                             <Card.Text>
                                 <input className='loginInput passwordInput' id='passwordInput' type='password'/>
                             </Card.Text>
-                            <Link to='/e-commerce/Store' className='btn btn-outline-primary' onClick={() => this.setAccountLocal()}>Submit</Link>
+                            <Link to='/e-commerce/Store' className='btn btn-outline-primary' onClick={() => this.setAccount()}>Submit</Link>
                         </Form>
                     </Card.Body>
                 </Card>
@@ -44,23 +44,6 @@ export class Login extends Component {
         localStorage.setItem('user', JSON.stringify(username));
         document.getElementById('FooterAccountName').innerHTML = 'Logout from: ' + store.getState().UserName;
         document.getElementById('FooterAccountName').setAttribute('href', '/e-commerce/');
-    }
-    setAccountLocal() {
-        let localName = JSON.parse(localStorage.getItem('user'));
-        if (localName === undefined) {
-            this.setAccount();
-        } else {
-            if (store.getState().UserName === undefined) {
-                let username = localName;
-                store.dispatch({type: 'ACCOUNT_NAME', username});
-                localStorage.setItem('user', JSON.stringify(username));
-                document.getElementById('FooterAccountName').innerHTML = 'Logout from: ' + store.getState().UserName;
-                document.getElementById('FooterAccountName').setAttribute('href', '/e-commerce/');
-            } else {
-                document.getElementById('FooterAccountName').innerHTML = 'Logout from: ' + store.getState().UserName;
-                document.getElementById('FooterAccountName').setAttribute('href', '/e-commerce/');
-            }
-        }
     }
     render() {
         return(
